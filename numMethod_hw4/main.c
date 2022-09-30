c9bfea75bb7206ee44dd3e2be0305633299622a7
Irokanade <michaeljs.leong@gmail.com>
2022-09-30 14:18:37 +0800

    Initial Commit

---
+++ numMethod_hw4/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  numMethod_hw4
+//
+//  Created by Michael Leong on 30/09/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
